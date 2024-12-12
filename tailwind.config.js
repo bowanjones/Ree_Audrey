618015ffe2bd15574b6eb6f332e9bc6f2988199e
bowanjones <158227679+bowanjones@users.noreply.github.com>
2024-12-12 11:52:04 -0600

    Trying to add files
    1

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: "selector",
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      keyframes: {
+        wavey: {
+          "0%, 100%": {
+            transform: "scaleY(0.5)"
+          },
+          "50%": {
+            transform: "scaleY(1.5)"
+          }
+        }
+      },
+      // animation-wavey
+      animation: {
+        wavey: "wavey 1500ms linear infinite"
+      }
+    },
+  },
+  plugins: [
+    require("./plugins/openVariant"), 
+    require("./plugins/animationDelay"),
+    require("./plugins/tableCaption"),
+    require("./plugins/buttonPlugin"),
+    require("./plugins/diagonalRadius"),
+  ], 
+}
